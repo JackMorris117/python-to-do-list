f05ad900d3ec920ee371c7cf69208d920ff96e04
Jack Morris <morriseo94@gmail.com>
2020-10-07 11:45:21 +0100

    groceries visible

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,17 @@
+from flask import render_template
 from app import app
 
 
-@app.route("/<name>")
-def home(name):
-    return f"Hello {name.upper()}!"
+@app.route("/")
+def home():
+    user = {"username": "Jack"}
+    tasks = [
+        {
+            "id": 1,
+            "title": "Buy groceries",
+            "description": "Milk, cheese, pizza, fruit",
+            "done": True
+        }
+    ]
+    return render_template("index.html", title="Home", user=user, tasks=tasks)
 
