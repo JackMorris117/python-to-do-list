b44b34bb64816a3feae00e82ccb06e92f468c433
Jack Morris <morriseo94@gmail.com>
2020-10-07 14:56:20 +0100

    db

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,10 @@
 from flask import render_template
 from app import app
+from app.models import User, Task
 
-
-@app.route("/")
-def home():
-    user = {"username": "Jack"}
-    tasks = [
-        {
-            "id": 1,
-            "title": "Buy groceries",
-            "description": "Milk, cheese, pizza, fruit",
-            "done": True
-        }
-    ]
+@app.route("/tasks")
+def index():
+    user = User.query.get(1)
+    tasks = user.tasks
     return render_template("index.html", title="Home", user=user, tasks=tasks)
 
